2967c2e99bde9c3e74b764f045e6e52d2e0225de
Richard <richard.williams2@man.ac.uk>
2015-05-27 16:00:58 +0100

    Minor mods after review.

--- script.js
+++ script.js
@@ -15,10 +15,33 @@
     }
   };
 
+  var getDateString = function(date){
+    var m = date.getMonth()+1;
+    var d = date.getDate();
+    return date.getFullYear() + "-" + (m<10 ? "0" : "")+m+"-"+(d<10 ? "0" : "")+d;
+  };
+
+  var slideUpCharts = function(){
+    var nextSlot = [];
+
+    //for each panel
+    $('.chart-panel').children().each(function(index, value){
+      if(index%2 !== 0) return;
+      if($(value).not(':has(*)').length>0){
+        nextSlot.push($(value).attr('id'));
+      } else if(nextSlot.length>0) {
+        var id = nextSlot[0];
+
+        $('#'+id).parent().before($(value).parent());
+      }
+    });
+  };
+
   var addChart = function(item) {
     destroyCharts([item + '-chart']);
 
     var id = $($('.chart-panel').children(':not(:has(*))')[0]).attr('id');
+    if(!id) return false;
     var chartOptions = {
       bindto: '#'+id,
       data: {
@@ -30,6 +53,13 @@
       },
       zoom: {
         enabled: true
+      },
+      tooltip:{
+        format: {
+          title: function(x) {
+            return getDateString(x) + " - Source: " + pb.data[item].sources[getDateString(x)];
+          }
+        }
       },
       legend: {
         show:false
@@ -78,6 +108,8 @@
     .style('font-size', '1.4em')
     .style('font-weight', '600')
     .text(pb.data[item].fullname + ' (' + pb.data[item].units +')');
+
+    return true;
   };
 
   pb.wireUpPages = function() {
@@ -97,11 +129,49 @@
       if($(this).hasClass('selected')){
         $(pb[item+'-chart'].element).siblings().html("");
         destroyCharts([item+'-chart']);
+        $(this).removeClass('selected');
       } else {
-        addChart(item);
+        if(addChart(item)) $(this).addClass('selected');
       }
-      $(this).toggleClass('selected');
+      slideUpCharts();
     });
+  };
+
+  var getTrend = function(values, name){
+    var x = ["x"];
+    var vals = [name];
+
+    for(var i = 0; i < values.length; i++){
+      x.push(values[i].date);
+      vals.push(values[i].value);
+    }
+
+    return [x, vals];
+  };
+
+  var getSources = function(values){
+    var x = {};
+
+    for(var i = 0; i < values.length; i++){
+      x[values[i].date] = values[i].source;
+    }
+
+    return x;
+  };
+
+  var getProps = function(values){
+    var min = values[0].value;
+    var max = values[0].value;
+    var tot = 0;
+
+    for(var i = 1; i < values.length; i++){
+      tot += values[i].value;
+      if(values[i].value < min) min = values[i].value;
+      if(values[i].value > max) max = values[i].value;
+    }
+    var mean = Math.round(100 * tot / values.length) / 100;
+
+    return {"mean" : mean, "max" : max, "min" : min};
   };
 
   pb.loadData = function(callback) {
@@ -112,14 +182,20 @@
       pb.abnormal = [];
       var index = 0;
       for (var o in file) {
+        file[o].name = o;
+        file[o].chartid = "chart-" + index++;
+        file[o].trend = getTrend(file[o].values, o);
+        file[o].date = file[o].trend[0][file[o].trend[0].length-1];
+        file[o].value = file[o].trend[1][file[o].trend[1].length-1];
+        file[o].source = file[o].values[file[o].values.length-1].source;
+        file[o].sources = getSources(file[o].values);
+        file[o].props = getProps(file[o].values);
         if (file[o].value >= file[o].normal.min && file[o].value <= file[o].normal.max) {
           pb.normal.push(file[o]);
         } else {
           pb.abnormal.push(file[o]);
         }
-        file[o].name = o;
-        file[o].chartid = "chart-" + index++;
-        file[o].date = file[o].trend[0][file[o].trend[0].length - 1];
+        file[o].inRange = (file[o].value >= file[o].normal.min && file[o].value <= file[o].normal.max ? "normal" : "abnormal");
         if (file[o].trend[1].length > 1) {
           file[o].change = Math.round((file[o].trend[1][file[o].trend[1].length - 1] - file[o].trend[1][file[o].trend[1].length - 2]) * 100) / 100;
         } else {
